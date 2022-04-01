18267f83ec23881d064621dfb3e648e5b26c4364
lingadhar <30472378+lingadhar@users.noreply.github.com>
2022-04-01 21:18:47 +0530

    Add files via upload

---
+++ DataIngest.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import string
+import random
+import sqlite3
+
+class data_ingest:
+    def __init__(self, file,tgt):
+        self._file = file
+        self._target = tgt
+
+    #Load the csv file
+    def excel_loader(self):
+        data = pd.read_csv(self._file)
+        return data
+
+    #Customer ID and Product ID generation
+    def id_generator(self, size=6, chars=string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+
+    def cust_prod_id_gen(self):
+        data = self.excel_loader()
+        custdict = dict()
+        for i in list(set(data['ClientName'].tolist())):
+            custdict[i] = "CL" + self.id_generator(6)
+
+        proddict = dict()
+        for i in list(set(data['ProductName'].tolist())):
+            proddict[i] = "PR" + self.id_generator(6)
+
+        data['Cust_id'] = ''
+        data['Product_id'] = ''
+        for ind, row in data.iterrows():
+            data.loc[ind, ['Cust_id']] = custdict[row['ClientName']]
+            data.loc[ind, ['Product_id']] = proddict[row['ProductName']]
+        return data
+
+    # Creation of dataframes from the csv data file
+    def data_ingest(self):
+        data = self.cust_prod_id_gen()
+        data_PO = data[['OrderNumber','Product_id','ProductQuantity']]
+        data_product = data[['Product_id','ProductName','ProductType','UnitPrice']]
+        data_Invoice = data[['PaymentBillingCode','PaymentDate','PaymentType','Currency']]
+        data_customer = data[['Cust_id','ClientName','DeliveryAddress','DeliveryCity','DeliveryPostcode','DeliveryCountry','DeliveryContactNumber']]
+        data_fact = data[['Cust_id','OrderNumber','Product_id','PaymentBillingCode','ProductQuantity','TotalPrice']]
+
+        # Dropping duplicates from the required dataframes
+        data_customer = data_customer.drop_duplicates()
+        data_Invoice = data_Invoice.drop_duplicates()
+        data_product = data_product.drop_duplicates()
+
+        # Copy the dataframes to SQLite tables
+        try:
+            conn = sqlite3.connect(self._target)
+            data_PO.to_sql('Purchase_Order', conn, if_exists='append', index=False)
+            data_product.to_sql('Product', conn, if_exists='append', index=False)
+            data_Invoice.to_sql('Invoice', conn, if_exists='append', index=False)
+            data_customer.to_sql('Customer', conn, if_exists='append', index=False)
+            data_fact.to_sql('fact', conn, if_exists='append', index=False)
+        except Exception as e:
+            print("error")
+            raise (str(e))
---
+++ SQLite_Table_Creation.py
@@ -0,0 +1,108 @@
+#SQLite table creation
+
+import sqlite3
+from sqlite3 import Error
+from paths import *
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def main():
+    database = TGT_FOLDER + "\\"+"abcdb.db"
+    
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    sql_create_Purchase_Order = """ CREATE TABLE IF NOT EXISTS Purchase_Order (
+                                        OrderNumber varchar(50) PRIMARY KEY,
+                                        Product_id varchar(50) NOT NULL,
+                                        ProductQuantity int NOT NULL,
+                                        FOREIGN KEY (Product_id) REFERENCES Product (Product_id)
+                                    ); """
+    sql_create_Invoice = """CREATE TABLE IF NOT EXISTS Invoice (
+                                    PaymentBillingCode varchar(50) PRIMARY KEY,
+                                    PaymentDate date NOT NULL,
+                                    PaymentType text NOT NULL,
+                                    Currency char(5) NOT NULL
+                                );"""
+    
+    sql_create_Product = """CREATE TABLE IF NOT EXISTS Product (
+                                    Product_id varchar(50) PRIMARY KEY,
+                                    UnitPrice integer NOT NULL,
+                                    ProductName text,
+                                    ProductType text
+                                );"""
+    
+    sql_create_Customer = """CREATE TABLE IF NOT EXISTS Customer (
+                                    Cust_id varchar(50) PRIMARY KEY,
+                                    ClientName text,
+                                    DeliveryAddress text,
+                                    DeliveryCity text,
+                                    DeliveryPostcode text,
+                                    DeliveryCountry text,
+                                    DeliveryContactNumber text
+                                );"""
+    
+    sql_create_fact = """CREATE TABLE IF NOT EXISTS fact (
+                                    Cust_id varchar(50),
+                                    OrderNumber varchar(50),
+                                    Product_id varchar(50),
+                                    PaymentBillingCode varchar(50),
+                                    ProductQuantity int,
+                                    TotalPrice int,
+                                    FOREIGN KEY (Cust_id) REFERENCES Customer (Cust_id),
+                                    FOREIGN KEY (Product_id) REFERENCES Product (Product_id),
+                                    FOREIGN KEY (PaymentBillingCode) REFERENCES Invoice (PaymentBillingCode),
+                                    FOREIGN KEY (OrderNumber) REFERENCES Purchase_Order (OrderNumber)
+                                );"""
+
+    if conn is not None:
+        # create Purchase_Order table
+        create_table(conn, sql_create_Purchase_Order)
+
+        # create Invoice table
+        create_table(conn, sql_create_Invoice)
+        
+        # create Product table
+        create_table(conn, sql_create_Product)
+        
+        # create Customer table
+        create_table(conn, sql_create_Customer)
+        
+        # create Fact table
+        create_table(conn, sql_create_fact)
+        
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
